f42f526065a15c2c7335b0c5d588861866830aaa
Theofilos Intzoglou <int.teo@gmail.com>
2023-09-17 00:10:26 +0300

    Εμφάνισε τις σημειώσεις

---
+++ resources/ts/Pages/Note/Index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1 class="text-2xl font-bold">Σημειώσεις</h1>
+        <ul v-if="notes" class="list-disc list-inside">
+            <li v-for="note in notes" :key="note.id" >
+                <div class="text-lg inline-block">{{ note.title }}</div>
+                <div>{{ note.content }}</div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+    notes: Array<App.Models.Note>
+}>();
+</script>
