654c1df27553a0412d62b9bea997ae88f6a28af5
Theofilos Intzoglou <int.teo@gmail.com>
2023-09-17 18:53:09 +0300

    Υλοποίηση διαγραφής σημειώσεων

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -73,6 +73,8 @@ class NoteController extends Controller
      */
     public function destroy(Note $note)
     {
-        //
+        $note->delete();
+
+        return to_route('note.index');
     }
 }
