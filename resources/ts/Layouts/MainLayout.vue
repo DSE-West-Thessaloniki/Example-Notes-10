e8e8f5195a9649592b249bf6d3f8842c33fa507f
Theofilos Intzoglou <int.teo@gmail.com>
2023-09-18 19:26:11 +0300

    Εμφάνιση προφίλ χρήστη

--- resources/ts/Layouts/MainLayout.vue
+++ resources/ts/Layouts/MainLayout.vue
@@ -7,9 +7,120 @@
                 <Link href="/" class="text-lg font-bold">Σημειώσεις</Link>
                 <div v-if="$page.props.canLogin" class="d-flex justify-content-end">
                     <div>
-                        <Link :href="route('logout')" method="post">
-                        Αποσύνδεση
-                        </Link>
+                        <!-- Settings Dropdown -->
+                        <div class="ml-3 relative">
+                            <Dropdown align="right" width="48">
+                                <template #trigger>
+                                    <button v-if="$page.props.jetstream.managesProfilePhotos" class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
+                                        <img class="h-8 w-8 rounded-full object-cover" :src="$page.props.auth.user.profile_photo_url" :alt="$page.props.auth.user.name">
+                                    </button>
+
+                                    <span v-else class="inline-flex rounded-md">
+                                        <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
+                                            {{ $page.props.auth.user.name }}
+
+                                            <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                            </svg>
+                                        </button>
+                                    </span>
+                                </template>
+
+                                <template #content>
+                                    <!-- Account Management -->
+                                    <div class="block px-4 py-2 text-xs text-gray-400">
+                                        Manage Account
+                                    </div>
+
+                                    <DropdownLink :href="route('profile.show')">
+                                        Profile
+                                    </DropdownLink>
+
+                                    <DropdownLink v-if="$page.props.jetstream.hasApiFeatures" :href="route('api-tokens.index')">
+                                        API Tokens
+                                    </DropdownLink>
+
+                                    <div class="border-t border-gray-200" />
+
+                                    <!-- Authentication -->
+                                    <form @submit.prevent="logout">
+                                        <DropdownLink as="button">
+                                            Log Out
+                                        </DropdownLink>
+                                    </form>
+                                </template>
+                            </Dropdown>
+                        </div>
+
+                        <!-- Hamburger -->
+                        <div class="-mr-2 flex items-center sm:hidden">
+                            <button class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out" @click="showingNavigationDropdown = ! showingNavigationDropdown">
+                                <svg
+                                    class="h-6 w-6"
+                                    stroke="currentColor"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                >
+                                    <path
+                                        :class="{'hidden': showingNavigationDropdown, 'inline-flex': ! showingNavigationDropdown }"
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        stroke-width="2"
+                                        d="M4 6h16M4 12h16M4 18h16"
+                                    />
+                                    <path
+                                        :class="{'hidden': ! showingNavigationDropdown, 'inline-flex': showingNavigationDropdown }"
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        stroke-width="2"
+                                        d="M6 18L18 6M6 6l12 12"
+                                    />
+                                </svg>
+                            </button>
+                        </div>
+
+                        <div :class="{'block': showingNavigationDropdown, 'hidden': ! showingNavigationDropdown}" class="sm:hidden">
+                            <div class="pt-2 pb-3 space-y-1">
+                                <ResponsiveNavLink :href="route('dashboard')" :active="route().current('dashboard')">
+                                    Dashboard
+                                </ResponsiveNavLink>
+                            </div>
+
+                            <!-- Responsive Settings Options -->
+                            <div class="pt-4 pb-1 border-t border-gray-200">
+                                <div class="flex items-center px-4">
+                                    <div v-if="$page.props.jetstream.managesProfilePhotos" class="shrink-0 mr-3">
+                                        <img class="h-10 w-10 rounded-full object-cover" :src="$page.props.auth.user.profile_photo_url" :alt="$page.props.auth.user.name">
+                                    </div>
+
+                                    <div>
+                                        <div class="font-medium text-base text-gray-800">
+                                            {{ $page.props.auth.user.name }}
+                                        </div>
+                                        <div class="font-medium text-sm text-gray-500">
+                                            {{ $page.props.auth.user.email }}
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="mt-3 space-y-1">
+                                    <ResponsiveNavLink :href="route('profile.show')" :active="route().current('profile.show')">
+                                        Profile
+                                    </ResponsiveNavLink>
+
+                                    <ResponsiveNavLink v-if="$page.props.jetstream.hasApiFeatures" :href="route('api-tokens.index')" :active="route().current('api-tokens.index')">
+                                        API Tokens
+                                    </ResponsiveNavLink>
+
+                                    <!-- Authentication -->
+                                    <form method="POST" @submit.prevent="logout">
+                                        <ResponsiveNavLink as="button">
+                                            Log Out
+                                        </ResponsiveNavLink>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -23,9 +134,19 @@
 <script setup lang="ts">
 import { Head, Link } from '@inertiajs/vue3'
 import route from 'ziggy-js';
+import Dropdown from '@/Components/Dropdown.vue';
+import DropdownLink from '@/Components/DropdownLink.vue';
+import NavLink from '@/Components/NavLink.vue';
+import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
+import { ref } from 'vue';
 
 const props = defineProps<{
     title: string
 }>();
 
+const showingNavigationDropdown = ref(false);
+
+const logout = () => {
+    router.post(route('logout'));
+};
 </script>
